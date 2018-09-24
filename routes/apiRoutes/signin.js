88fa5b879c50c26069a3023817a085a0968779e1
Wulfsbach <etemelkos@gmail.com>
2018-09-24 15:40:53 -0400

    working on signin.js and registration fields

--- routes/apiRoutes/signin.js
+++ routes/apiRoutes/signin.js
@@ -1,3 +1,56 @@
 module.exports = (app) => {
 
-};
+
+
+
+app.post('api/account/signup',(req,res,next) => {
+
+const { body } = req;
+const {
+    fistName,
+    lastName,
+    email,
+    password,
+    location
+} = body;
+
+if (!firstName) {
+    res.end({
+        success: false,
+        message: 'First Name cannot be left blank.'
+    });
+}
+    if (!lastName) {
+        res.end({
+            success: false,
+            message: 'Last Name cannot be left blank.'
+        });
+    }
+        if (!email) {
+            res.end({
+                success: false,
+                message: ' Email cannot be left blank.'
+            });
+        }
+            if (!password) {
+                res.end({
+                    success: false,
+                    message: 'First Name cannot be left blank.'
+                });
+            }
+                if (!location) {
+                    res.end({
+                        success: false,
+                        message: 'location cannot be left blank.'
+                    });
+
+                }
+
+                email = email.toLowerCase();
+            });
+
+        };
+
+
+
+
