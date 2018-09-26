69ca790e9a2950a521af27eba33bc190cab0aab2
Wulfsbach <etemelkos@gmail.com>
2018-09-25 20:33:02 -0400

    react home.js for sign-in and sign-up almost complete, buttons and inputs created.

--- view/my-react-app/src/components/home/home.js
+++ view/my-react-app/src/components/home/home.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import 'whatwg-fetch';
 
+import {
+    getFromStorage,
+    setInStorage,
+} from "../../../app.js/utils/storage";
 
 class Home extends Component {
     constructor(props) {
@@ -8,13 +12,39 @@ class Home extends Component {
 
         this.state ={
             isLoading: true,
+            token: '',
             signUpError:'',
             signInError:''
+            
         };
     }
 
     componentDidMount(){
-        fetch('/api/')
+        const token = getFromStorage('my-react-app');
+        if(token) {
+
+            fetch('/api/account/verify?token=' + token)
+            .then(res => res.json())
+            .then(json => {
+                if(json.success) {
+                    this.setState({
+                        token,
+                        isLoading:false
+                    });
+
+                }else {
+                    this.setState({
+                        isLoading:false,
+                    });
+                }
+            });
+        
+        
+        }else{
+            this.setState({
+                isLoading: false,
+            });
+        }
     }
 
 
@@ -22,15 +52,42 @@ class Home extends Component {
     render() {
         const{
             isLoading,
+            token,
         } =this.state;
 
         if (isLoading){
-            reutrn(<div><p>Loading...</p></div>);
+            return(<div><p>Loading...</p></div>);
         }
+    if (!token) {
+        return (
+        
+        <div>
+          <div>
+      
+
+<p>Sign In </p>
+<input type="email" placeholder="Email" />
+<input type="password" placeholder="Password"/>
+<button>Sign In</button>
+</div>
+<br />
+<br />
+<div>
+      <p>Sign Up</p>
+    <input type="text" placeholder="First Name" />< br />
+    <input type="text" placeholder="Last Name" />< br />
+    <input type="text" placeholder="Location/City" />< br />
+      <input type="email" placeholder="Email" />< br />
+      <input type="password" placeholder="Password"/>< br />
+      <button>Sign Up </button>
+      </div>
+            </div>
+            );
     }
     return(
         <div>
-
+<p> Account </p>
         </div>
     );
+    }
 }
